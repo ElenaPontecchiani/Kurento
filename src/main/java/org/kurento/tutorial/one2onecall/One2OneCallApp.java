1d8cebd09f1f48acb6523b6a800faaaf77a77568
Elena Pontecchiani <ele.pontecchiani@gmail.com>
2019-09-19 16:08:51 +0200

    chiamata 1 a 1

--- src/main/java/org/kurento/tutorial/one2onecall/One2OneCallApp.java
+++ src/main/java/org/kurento/tutorial/one2onecall/One2OneCallApp.java
@@ -1,20 +1,21 @@
 /*
- * Copyright 2018 Kurento (https://www.kurento.org)
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package org.kurento.tutorial.helloworld;
+package org.kurento.tutorial.one2onecall;
 
 import org.kurento.client.KurentoClient;
 import org.springframework.boot.SpringApplication;
@@ -25,32 +26,37 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 /**
- * Kurento Java Tutorial - Application entry point.
+ * Video call 1 to 1 demo (main).
+ *
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @author Micael Gallego (micael.gallego@gmail.com)
+ * @since 4.3.1
  */
 @SpringBootApplication
 @EnableWebSocket
-public class Application implements WebSocketConfigurer
-{
+public class One2OneCallApp implements WebSocketConfigurer {
+
   @Bean
-  public Handler handler()
-  {
-    return new Handler();
+  public CallHandler callHandler() {
+    return new CallHandler();
   }
 
   @Bean
-  public KurentoClient kurentoClient()
-  {
+  public UserRegistry registry() {
+    return new UserRegistry();
+  }
+
+  @Bean
+  public KurentoClient kurentoClient() {
     return KurentoClient.create();
   }
 
   @Override
-  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry)
-  {
-    registry.addHandler(handler(), "/helloworld");
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(callHandler(), "/call");
   }
 
-  public static void main(String[] args) throws Exception
-  {
-    SpringApplication.run(Application.class, args);
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(One2OneCallApp.class, args);
   }
 }
